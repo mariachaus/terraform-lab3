cca4f7474a8e1b3449013f450dbbb7d6cdc3707d
marii <maria2607july@gmail.com>
2026-03-19 10:09:49 +0200

    Add main.tf with core infrastructure resources

--- main.tf
+++ main.tf
@@ -1,17 +1,4 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 5.0.0"
-    }
-  }
-}
 
-provider "google" {
-  project = var.project
-  region  = var.region
-  zone    = var.zone
-}
 
 # Глобальні теги для всіх ресурсів
 locals {
